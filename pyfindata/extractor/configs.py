550e8917db81029e22219f87e6778a543eea7a53
wegar2 <awegrzyn17@gmail.com>
2025-01-10 15:35:16 +0100

    rewritten stooq files data extraction extractor using new configs

---
+++ pyfindata/extractor/configs.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from pydantic import BaseModel, ConfigDict, field_validator
+
+from pyfindata.common.ts_config import TSConfig
+
+
+class StooqCsvFileConfig(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    ts_config: TSConfig
+    filepath: Path
+
+    @field_validator("filepath", mode="before") # noqa
+    @classmethod
+    def _validate_filepath(cls, value):
+        if not value.name.endswith(".csv"):
+            raise ValueError(
+                f"Invalid filepath {value} encountered when creating "
+                f"{cls.__name__} instance: not a CSV file path"
+            )
+        return value
--- pyfindata/extractor/stooq_csv_extractor.py
+++ pyfindata/extractor/stooq_csv_extractor.py
@@ -1,11 +1,10 @@
-from functools import reduce
-from pathlib import Path
-from typing import Final, Union
+from typing import Union
 
 from loguru import logger
 import pandas as pd
 
-from pyfindata.common.constants import DEFAULT_DATA_PATH
+from pyfindata.common.ts_data import TSData
+from pyfindata.extractor.configs import StooqCsvFileConfig
 from pyfindata.extractor.constants import StooqDataModel
 
 
@@ -13,44 +12,37 @@ class StooqCsvExtractor:
 
     def __init__(
             self,
-            files: Union[str, list[str]],
-            folder: Path = DEFAULT_DATA_PATH,
+            csv_configs: Union[StooqCsvFileConfig, list[StooqCsvFileConfig]],
     ):
-        if not folder.is_dir():
-            raise Exception(f"Path {folder} does not point at a directory")
-        self._folder: Final[Path] = folder
+        if isinstance(csv_configs, StooqCsvFileConfig):
+            csv_configs = [StooqCsvFileConfig]
+        self._validate_csv_configs(csv_configs=csv_configs)
+        self._csv_configs: list[StooqCsvFileConfig] = csv_configs
 
-        if isinstance(files, str):
-            files = [files]
-        self._validate_files(files=files)
-        self._files: list[str] = files
-
-    def _validate_files(self, files: list[str]):
-        folder_files: list[str] = [
-            x.name[:-4] for x in self._folder.glob("*.csv")
-        ]
+    @staticmethod
+    def _validate_csv_configs(csv_configs: list[StooqCsvFileConfig]):
         if len(missing_files := [
-            x for x in files if x not in folder_files
+            cfg.filepath
+            for cfg in csv_configs
+            if not cfg.filepath.exists() or not cfg.filepath.is_file()
         ]) > 0:
-            raise Exception(f"Files: {', '.join(missing_files)} are not "
-                            f"available in the data folder! ")
+            raise Exception(
+                f"Files: "
+                f"{', '.join([str(file) for file in missing_files])}"
+                f" either don't exist or are not files! "
+            )
 
-    def extract(self) -> pd.DataFrame:
-        datas: list[pd.DataFrame] = []
-        for file in self._files:
-            with self._folder / f"{file}.csv" as fpath:
-                logger.info(f"Extracting data ")
-                data = pd.read_csv(fpath)
-                data: pd.DataFrame = pd.DataFrame(StooqDataModel.validate(data))
-                data = data.set_index("Date")
-                data = data[["Close"]]
-                data = data.rename(columns={"Close": file})
-                datas.append(data)
-        data = reduce(
-            lambda left, right: pd.merge(
-                left=left, right=right, left_index=True, right_index=True,
-                how="outer"
-            ),
-            datas
-        )
-        return data.ffill(axis=0).bfill(axis=0)
+    def extract(self) -> list[TSData]:
+        ts_datas: list[TSData] = []
+        for csv_config in self._csv_configs:
+            logger.info(f"Extracting & processing data "
+                        f"from file {csv_config.filepath}")
+            data = pd.read_csv(csv_config.filepath)
+            data: pd.DataFrame = pd.DataFrame(StooqDataModel.validate(data))
+            data = data.set_index("Date")
+            data = data[["Close"]]
+            data = data.rename(columns={"Close": csv_config.ts_config.ticker})
+            ts_datas.append(
+                TSData(data=data, ts_config=csv_config.ts_config)
+            )
+        return ts_datas
