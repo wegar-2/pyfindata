b7d6026b3001a89a4c8d0f1adeb5b55a1c7e352c
wegar2 <awegrzyn17@gmail.com>
2025-01-10 19:40:27 +0100

    Added skeleton of FRED API data extractor

---
+++ pyfindata/extractor/fred_extractor.py
@@ -0,0 +1,16 @@
+from typing import Final, Optional
+
+import pandas as pd
+
+
+class FredDBExtractor:
+
+    def __init__(self, api_key: Optional[str] = None):
+        pass
+
+    def extract(self) -> pd.DataFrame:
+        pass
+
+
+if __name__ == "__main__":
+    pass
